cef6bed1c350c2eeaf569a13dbb6a2a82d0ee7e3
rigoood <132658372+rigoood@users.noreply.github.com>
2023-08-23 20:12:18 +0900

    Update BOJ 1697.py

--- 규리/BOJ 1697.py
+++ 규리/BOJ 1697.py
@@ -2,22 +2,19 @@
 
 from collections import deque
 
-def f(N):
-    global time
+def bfs(N):
     visited = [0]*100001
     q = deque()
     q.append(N)
+
     while q:
         n = q.popleft()
         if n == K:
-            result = visited[n]
-            break
-        for k in [n-1, n+1, n*2]:
-            if 0 <= k <= 100000 and visited[k] == 0:
-                visited[k] = visited[n] + 1
-                q.append(k)
-    return result
+            return visited[n]
+        for i in [n-1, n+1, 2*n]:
+            if 0 <= i <= 100000 and visited[i] == 0:
+                visited[i] = visited[n] + 1
+                q.append(i)
 
 N, K = map(int, input().split())
-time = 0
-print(f(N))
+print(bfs(N))
