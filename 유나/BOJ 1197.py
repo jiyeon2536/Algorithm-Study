0bcde64a6d882d26e0ee471fe210f396c5b2ea7d
AHN YUNA <yunanash1234@gmail.com>
2023-09-22 14:57:15 +0900

    [BOJ] 1197. 최소 스패닝 트리

--- 유나/BOJ 1197.py
+++ 유나/BOJ 1197.py
@@ -10,6 +10,7 @@ for _ in range(E):
     graph[u].append((v, w))
     graph[v].append((u, w))
 
+# Prim
 def prim(graph, start):
     visited = [False] * (V+1) # 방문 체크열
     min_heap = [(0, start)] # 첫 시작 정점의 가중치는 0
@@ -36,4 +37,45 @@ total = 0
 for node, cost in MST:
     total += cost
 
+print(total)
+
+# kruskal
+def find(parents, x):
+    if x == parents[x]:
+        return x
+    parents[x] = find(parents, parents[x] )
+    return parents[x]
+
+def union(parents, x, y):
+    px = find(parents, x)
+    py = find(parents, y)
+
+    if px != py:
+        parents[py] =px
+
+def kruskal(graph):
+    edges = []
+    for u in range(V+1):
+        for v, w in graph[u]:
+            edges.append((u, v, w))
+
+    edges.sort(key = lambda x :x[2])
+    MST = []
+
+    parents = [i for i in range(V+1)]
+
+    for u, v, w in edges:
+        # 각 노드의 조상이 다르다면, 연결되지 않았음을 의미
+        if find(parents, u) != find(parents, v):
+            # MST에 추가
+            MST.append((u, v, w))
+            # 연결해줘!
+            union(parents, u, v)
+    return MST
+
+MST = kruskal(graph)
+
+total = 0
+for u, v, w in MST:
+    total += w
 print(total)
