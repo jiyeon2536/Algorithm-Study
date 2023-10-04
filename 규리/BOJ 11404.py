ad2074bac92afc22168ab5b775a3c06ad7360763
rigoood <132658372+rigoood@users.noreply.github.com>
2023-10-04 20:41:27 +0900

    [BOJ] 11404. 플로이드

--- 규리/BOJ 11404.py
+++ 규리/BOJ 11404.py
@@ -8,6 +8,7 @@ graph = [[] for _ in range(V+1)]
 for _ in range(E):
     u, v, w = map(int, input().split())
     check = False
+    # u -> v 최소비용 w 저장
     for x in range(len(graph[u])):
         if graph[u][x][0] == v:
             check = True
@@ -19,21 +20,26 @@ for _ in range(E):
 def floyd_warshall(graph):
     dist = [[float('inf')] * (V+1) for _ in range(V+1)]
 
+    # i -> i : 최소비용 0
     for i in range(V+1):
         dist[i][i] = 0
 
+    # 인접리스트 간선 정보로 최소비용 갱신
     for u in range(V+1):
         for v, w in graph[u]:
             dist[u][v] = w
 
+    # 최소비용 갱신 시도
     for k in range(V+1):
         for i in range(V+1):
             for j in range(V+1):
+                # i -> j 비용 > i -> k -> j
                 if dist[i][j] > dist[i][k] + dist[k][j]:
                     dist[i][j] = dist[i][k] + dist[k][j]
     return dist
 
 shortest_dist = floyd_warshall(graph)
+# 갈 수 없으면 0
 for i in range(1, len(shortest_dist)):
     for j in range(1, len(shortest_dist[i])):
         if shortest_dist[i][j] == float('inf'):
