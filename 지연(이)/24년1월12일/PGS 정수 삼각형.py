5af6b84919b22948d57e0a8ad1665284c7fdfd59
지연 <139419091+jiyeonnnny@users.noreply.github.com>
2024-01-12 21:28:19 +0900

    Create PGS 정수 삼각형.py

---
+++ 지연(이)/24년1월12일/PGS 정수 삼각형.py
@@ -0,0 +1,18 @@
+from collections import deque 
+
+def solution(triangle):
+    N = len(triangle)
+    answer = 0
+    
+    
+    for i in range(1,N):
+        for j in range(len(triangle[i])):
+            if j==0:
+                triangle[i][j] = triangle[i-1][j] + triangle[i][j]
+            elif j==len(triangle[i])-1:
+                triangle[i][j] = triangle[i-1][j-1] + triangle[i][j]
+            else:
+                triangle[i][j] = max(triangle[i-1][j-1],triangle[i-1][j]) + triangle[i][j]
+    
+    answer = max(triangle[N-1])
+    return answer
