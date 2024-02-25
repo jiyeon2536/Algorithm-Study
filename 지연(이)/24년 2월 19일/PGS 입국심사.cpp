f732c02e310ec873c77c2249522515089fd1180f
지연 <139419091+jiyeonnnny@users.noreply.github.com>
2024-02-26 08:59:10 +0900

    Create PGS 입국심사.cpp

---
+++ 지연(이)/24년 2월 19일/PGS 입국심사.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 1e10; // max
+
+    long long start = 0;
+    long long end = 0;
+    long long mid;
+    long long people = 0;
+    for (auto time : times) {
+        if (end < time) end = time;
+    }
+    end *= n;
+
+    while (start <= end) {
+        mid = (start + end) / 2;
+        people = 0;
+        for (int i = 0; i < times.size(); i++) {
+            people += mid / times[i];
+        }
+
+        if ( people < n ) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+            answer = mid;
+            // if (answer > mid) answer = mid;
+        }
+    }
+    return answer;
+}
